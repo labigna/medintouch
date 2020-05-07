40c3d078a293c34e4019c27a74f6ae3d746dda1b
Labigna Traoré <labigna@hotmail.com>
2020-05-07 13:14:01 +0200

    Initial Commit

---
+++ medintouch/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  medintouch
+//
+//  Created by Labigna Traoré on 07/05/2020.
+//  Copyright © 2020 Labigna Traoré. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
