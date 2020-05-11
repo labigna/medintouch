60179aa2831e081b1e7fd0d23261697400c7c28c
abou hadji <abou.hadji@outlook.com>
2020-05-11 16:26:20 +0200

    creation contact doc view

---
+++ medintouch/ContactDocView.swift
@@ -0,0 +1,21 @@
+//
+//  ContactDocView.swift
+//  medintouch
+//
+//  Created by abouhadji on 11/05/2020.
+//  Copyright © 2020 Labigna Traoré. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContactDocView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ContactDocView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContactDocView()
+    }
+}
