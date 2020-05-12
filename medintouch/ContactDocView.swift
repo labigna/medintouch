f96e0d5eef0e7f2bc3848f6f1bffbfeb92708bdc
abou hadji <abou.hadji@outlook.com>
2020-05-12 09:37:36 +0200

    mise en place

--- medintouch/ContactDocView.swift
+++ medintouch/ContactDocView.swift
@@ -10,7 +10,60 @@ import SwiftUI
 
 struct ContactDocView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+//fond
+                    ZStack{
+                        Image("fond1")
+                            .resizable()
+                            .edgesIgnoringSafeArea(.all)
+                        
+                        Image ("avatar")
+                        .resizable()
+                        .frame(width: 220.0, height: 210.0)
+                            .padding(.bottom, 300.0)
+                        
+                        VStack{
+                            
+                            Text("Torrel Yannick")
+                                .padding(.top, 141.0)
+                            
+                            Text ("Homéopathe")
+                                     .font(.title)
+HStack{
+                                 Text("12 Place de gaulle 13001 Marseille 07 98 65 98 72  03 54 65 78 98.")
+                                     .font(.subheadline)
+                                     .foregroundColor(Color.black)
+                                    
+                                  
+    
+    Text("Consultations                Du lundi au vendredi De 9h à 17h")
+        .multilineTextAlignment(.leading)
+    
+                                    }
+
+                            HStack{
+// phone
+                                RoundedRectangle(cornerRadius: 10)
+                                .padding(.trailing, -30.0)
+                                .frame(width: 100.0, height: 35.0)
+                                .foregroundColor(Color.purple)
+                                .overlay(
+                                    Image("phone")
+                                .foregroundColor(.white))
+// rdv
+                                    RoundedRectangle(cornerRadius: 10)
+                                .padding(.trailing, -30.0)
+                                .frame(width: 100.0, height: 35.0)
+                                .foregroundColor(Color.purple)
+                                .overlay(
+                                            Text("RDV")
+                                .fontWeight(.bold)
+                                .foregroundColor(.white))
+                                }
+                                .padding( [.top, .leading], 60.0)
+                            
+                            }
+                           
+                        }
     }
 }
 
@@ -19,3 +72,5 @@ struct ContactDocView_Previews: PreviewProvider {
         ContactDocView()
     }
 }
+ 
+
