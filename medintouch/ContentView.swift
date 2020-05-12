231de8524e9f70482c02c0529f5f2dd83a30f422
abou hadji <abou.hadji@outlook.com>
2020-05-12 14:28:18 +0200

    mise en place

--- medintouch/ContentView.swift
+++ medintouch/ContentView.swift
@@ -10,7 +10,53 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+                       ZStack{
+                                Image("fond1")
+                                    .resizable()
+                                    .edgesIgnoringSafeArea(.all)
+                               
+                                VStack{
+                                    Image ("thym")
+                                            .resizable()
+                                            .aspectRatio(contentMode: .fill)
+                                            .frame(width: 220.0, height: 210.0)
+                                            .mask(Circle())
+                                      
+                                    .shadow(color: Color.black.opacity(0.3), radius: 10, x: 10, y: 10)
+                                    .shadow(color: Color.white.opacity(0.10), radius: 10, x: -10, y: -10)
+        //NOM et Metier
+                                    Text("THYM")
+                                       .font(.title)
+                                        .padding()
+                           
+        HStack{
+        //    description
+                                         Text("Le thym connu pour ses vertus bienfaisantes contre la toux. une bonne infusion de thym,par exemple,calme facilement les épisodes de toux et rhinopharyngés.")
+                                             .font(.subheadline)
+                                            .fontWeight(.light)
+                                            .multilineTextAlignment(.center)
+                                            .padding(.all, 35.0)
+        }
+                                    
+       RoundedRectangle(cornerRadius: 20)
+        .fill(Color.offWhite)
+          .frame(width: 120.0, height: 120.0)
+
+        .overlay(
+            RoundedRectangle(cornerRadius: 20)
+                .stroke(Color.black, lineWidth: 4)
+                .blur(radius: 4)
+                .offset(x: 2, y: 2)
+                .mask(RoundedRectangle(cornerRadius: 20).fill(LinearGradient( gradient: Gradient(colors: [Color.black, Color.clear]), startPoint: .topLeading, endPoint:.bottomTrailing))))
+        .overlay(
+        RoundedRectangle(cornerRadius: 20)
+            .stroke(Color.white, lineWidth: 4)
+            .blur(radius: 4)
+            .offset(x: -2, y: -2)
+            .mask(RoundedRectangle(cornerRadius: 20).fill(LinearGradient( gradient: Gradient(colors: [Color.black, Color.clear]), startPoint: .bottomTrailing, endPoint:.topLeading))))
+                                    }
+                                }
+
     }
 }
 
