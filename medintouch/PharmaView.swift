3da4cc5e640d2d76d6e82644d809c15fdaaa0956
Labigna Traoré <labigna@hotmail.com>
2020-05-22 13:54:41 +0200

    merge ContactPharma

---
+++ medintouch/PharmaView.swift
@@ -0,0 +1,121 @@
+//
+//  PharmaView.swift
+//  medintouch
+//
+//  Created by Labigna Traoré on 07/05/2020.
+//  Copyright © 2020 Labigna Traoré. All rights reserved.
+//
+
+import SwiftUI
+
+struct PharmaView: View {
+    var body: some View {
+               ZStack{
+                                 Image("fond1")
+                                     .resizable()
+                                     .edgesIgnoringSafeArea(.all)
+                                
+                                 VStack{
+                                   
+                                    //Navigation bar
+                                    HStack{
+                                   
+                                    //Navigation bar
+                                
+                                    Circle()
+                                        .frame(width: 30.0, height: 30.0)
+                                        .foregroundColor(.white)
+                                    .overlay(Button(action: {},
+                                           label:
+                                        {Text("< ").foregroundColor(.gray)
+                                            .padding([.leading, .bottom], 2.0)
+                                            
+                                    }
+                                    ))
+//                                        titre
+                                        .shadow(color: Color.black.opacity(0.2), radius: 8, x: 5, y: 5)
+                                                        
+                                                Spacer()
+                                                Text("PHARMACIE").font(.title).bold()
+                                                    .padding(.trailing, 15.0)}
+                                        .padding([.leading, .bottom], 22.0)
+                                                    
+                                        .shadow(color: Color.black.opacity(0.2), radius: 8, x: 5, y: 5)
+                                     Image ("phototo")
+                                     
+                                             .resizable()
+                                             .aspectRatio(contentMode: .fill)
+                                             .frame(width: 220.0, height: 210.0)
+                                             .mask(Circle())
+                                       
+                                     .shadow(color: Color.black.opacity(0.3), radius: 10, x: 10, y: 10)
+                                     .shadow(color: Color.white.opacity(0.10), radius: 10, x: -10, y: -10)
+         //NOM et Metier
+                                     Text("PHARMACIE")
+                                        .font(.title)
+                                         .padding()
+                            
+                                     Text ("Herboriste Du Père Blaise")
+                                              .font(.title)
+                                         .padding(.vertical, -13.0)
+         HStack{
+         //    Contact
+                                          Text("4-6 Rue Meolan\ndu PèreBlaize\n13001 Marseille\n04 22 67 80 11")
+                                              .font(.subheadline)
+                                             .fontWeight(.light)
+                                             .multilineTextAlignment(.trailing)
+                                             .padding([.top, .bottom, .trailing], 35.0)
+         //barre de separation
+             Divider()
+                 .padding([.top, .leading], -27.0)
+                 .frame(width: -15.0, height: 67.0)
+             .multilineTextAlignment(.center)
+         //heure d'ouverture
+             Text("pereblaize.fr\nouverture:\n09:30–18:30")
+                 .font(.subheadline)
+                 .fontWeight(.light)
+                 .foregroundColor(Color.black)
+                 .multilineTextAlignment(.leading)
+                 .padding([.top, .leading], -18.0)
+         }
+         .padding(.leading, -26.0)
+
+                                     HStack{
+                                         
+         // Boutton phone
+                                         RoundedRectangle(cornerRadius: 10)
+                                         .padding(0.0)
+                                         .frame(width: 140.0, height: 35.0)
+                                         .foregroundColor(Color.purple)
+                                         .overlay(
+                                          Image("phone")
+                                         .foregroundColor(.white))
+                                         .shadow(color: Color.black.opacity(0.3), radius: 10, x: 10, y: 10)
+                                         .shadow(color: Color.white.opacity(0.10), radius: 10, x: -10, y: -10)
+         // boutton rdv
+                                         RoundedRectangle(cornerRadius: 10)
+                                         .frame(width: 140.0, height: 35.0)
+                                         .foregroundColor(Color.purple)
+                                         .overlay(
+                                                     Text("RDV")
+                                         .fontWeight(.bold)
+                                                         .foregroundColor(.black)
+                                                         .opacity(1.0)
+                                                         .multilineTextAlignment(.center))
+                                         .shadow(color: Color.black.opacity(0.3), radius: 10, x: 10, y: 10)
+                                         .shadow(color: Color.white.opacity(0.10), radius: 10, x: -10, y: -10)
+                                         }
+                                     .padding( 3.0)
+                                     
+                                     }
+                                    
+                                 }
+    }
+}
+
+struct PharmaView_Previews: PreviewProvider {
+    static var previews: some View {
+        PharmaView()
+    }
+}
+
